627fbca336c65abb429e78dae4a0284cc5ff35ed
Marco Scandroglio <oilgordnacsocram@gmail.com>
2023-02-16 01:49:29 -0500

    updated DML

--- DML.sql
+++ DML.sql
@@ -5,6 +5,7 @@
 
 -- Database Manipulation queries organized by page
 
+
 -------------------------------------------------------
 -- Machines Page
 -------------------------------------------------------
@@ -18,6 +19,9 @@ VALUES (:yearInput, :makeInput, :modelInput, :serialInput, :classInput);
 
 -- Update Machine in Machines Table
 UPDATE Machines SET year = :yearInput, make = :makeInput, model = :modelInput, serial = :serialInput, class = :classInput WHERE id= :machine_ID_from_the_update_form;
+
+-- Delete Machine in Machines Table
+DELETE FROM Machines WHERE machineId = :machineId_from_the_update_form;
 
 
 -------------------------------------------------------
@@ -81,53 +85,46 @@ VALUES (
   :descriptionInput
 );
 
+-- update existing work order
+UPDATE WorkOrders 
+SET machineId = (SELECT machineId FROM Machines WHERE serial = :machineSerialInput), 
+locationName = :locationNameInput, 
+date = :dateInput, 
+description= :descriptionInput 
+WHERE workOrderId = :workOrderId_from_the_update_form;
+
+-- Delete work order in WorkOrder Table
+DELETE FROM WorkOrders WHERE workOrderId = :workOrderId_from_the_update_form;
+
 -- populate machineSerial dropdown
 SELECT machineId from Machines;
 
 -- populate locationName dropdown
 SELECT locationId from Locations;
 
--- populate productReference dropdown
-SELECT productId from Products;
 
--- populate mechanicEmail dropdown
-SELECT mechanicId from Mechanics;
-
--- sample insert into WorkOrders
-INSERT INTO WorkOrders (machineId, locationId, date, description)
-VALUES (
-  (SELECT machineId FROM Machines WHERE serial = "50027"),
-  (SELECT locationId FROM Locations WHERE locationName = "Client Site 2"),
-  "2023-02-24",
-  "Machine needs an oil change"
-);
+-------------------------------------------------------
+-- Work Order Details Page
+-------------------------------------------------------
 
 -- add product to work order (M-to-M relationship addition)
 INSERT INTO WorkOrderProducts (workOrderId, productId) VALUES (workOrderId_from_input, :productId_from_input);
 
--- sample addition of product
-INSERT INTO WorkOrderProducts (workOrderId, productId)
-VALUES ((SELECT workOrderId FROM WorkOrders WHERE workOrderId = 10),
-        (SELECT productId FROM Products WHERE reference = "15W40"));
-
--- delete product from work order
+-- delete product from work order (M-to-M relationship deletion)
 DELETE FROM WorkOrderProducts WHERE workOrderId = :workOrderId_from_the_update_form AND (SELECT productId FROM Products WHERE reference = :referenceInput);
-
--- sample delete product from work order
-DELETE FROM WorkOrderProducts WHERE workOrderId = 10 AND (SELECT productId FROM Products WHERE reference = "15W40");
 
 -- add mechanic to work order (M-to-M relationship addition)
 INSERT INTO WorkOrderMechanics (workOrderId, mechanicId)
 VALUES ((SELECT workOrderId FROM WorkOrders WHERE workOrderId = :workOrderId_from_the_update_form),
         (SELECT mechanicId FROM Mechanics WHERE email=:emailInput));
 
--- sample add mechanic to work order
-INSERT INTO WorkOrderMechanics (workOrderId, mechanicId)
-VALUES (11, (SELECT mechanicId FROM Mechanics WHERE email="edisonjr@outlook.com"));
-
 -- delete mechanic from work order (M-to-M relationship deletion)
 DELETE FROM WorkOrderMechanics WHERE workOrderId = :workOrderId_from_the_update_form AND (SELECT mechanicId FROM Mechanics WHERE email=:emailInput);
 
+UPDATE WorkOrderProducts SET productId = :productId_from_input WHERE workOrderId = :workOrderId_from_the_update_form AND productId = :productId_from_the_update_form;
 
--- sample delete mechanic from work order
-DELETE FROM WorkOrderMechanics WHERE workOrderId = 11 AND (SELECT mechanicId FROM Mechanics WHERE email="edisonjr@outlook.com");
+-- populate productReference dropdown
+SELECT productId from Products;
+
+-- populate mechanicEmail dropdown
+SELECT mechanicId from Mechanics;
