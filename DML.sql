23b4825fbc84720747c77be5c3ba50a9e2fdd3d8
Marco Scandroglio <oilgordnacsocram@gmail.com>
2023-02-23 00:27:44 -0500

    updated DML to correlate with UI

--- DML.sql
+++ DML.sql
@@ -13,7 +13,8 @@
 -------------------------------------------------------
 
 -- Display Machines table on Machines page
-SELECT * FROM Machines;
+SELECT machineId AS Machine Id, year AS Year, make AS Make, model AS Model, serial AS Serial, class AS Class
+FROM Machines;
 
 -- Add machine to Machines table
 INSERT INTO Machines (year, make, model, serial, class) 
@@ -31,7 +32,8 @@ DELETE FROM Machines WHERE machineId = :machineId_from_the_update_form;
 -------------------------------------------------------
 
 -- Display Locations table on Locations page
-SELECT * FROM Locations;
+SELECT locationId AS Location Id,  locationName AS Location Name, address AS Adress, zipcode AS Zip Code, state AS State, isClientLocation AS Client Location?
+FROM Locations;
 
 -- add location to Locations table
 INSERT INTO Locations (locationName, address, zipcode, state, isClientLocation)
@@ -43,7 +45,8 @@ VALUES (:locationNameInput, :addressInput, :zipcodeInput, :stateInput, :isClient
 -------------------------------------------------------
 
 -- Display Mechanics table on Mechanics page
-SELECT * FROM Mechanics;
+SELECT mechanicId AS Mechanic Id, firstName AS First Name, lastName AS Last Name, phone AS Phone, email AS Email
+FROM Mechanics;
 
 -- add mechanic to Mechanics table
 INSERT INTO Mechanics (firstName, lastName, phone, email)
@@ -55,7 +58,8 @@ VALUES (:firstNameInput, :lastNameInput, :phoneInput, :emailInput);
 -------------------------------------------------------
 
 -- Display Products table on Products page
-SELECT * FROM Products;
+SELECT productId AS Product Id, productName AS Product Name, reference AS Reference, brand AS Brand, description AS Description
+FROM Products;
 
 -- add product to Products table
 INSERT INTO Products (productName, reference, brand, description)
@@ -68,12 +72,12 @@ VALUES (:productNameInput, :referenceInput, :brandInput, :descriptionInput);
 
 -- Display a join of WorkOrders, Machines, and Locations WorkOrders page
 -- shows a more complete description of each work order
-SELECT WorkOrders.workOrderId, 
-       Machines.model AS machineModel, 
-       Machines.serial AS machineSerial, 
-       Locations.locationName, 
-       WorkOrders.date, 
-       WorkOrders.description
+SELECT WorkOrders.workOrderId AS Order Id, 
+       Machines.model AS Machine Model, 
+       Machines.serial AS Machine Serial, 
+       Locations.locationName AS Location Name, 
+       WorkOrders.date AS Date, 
+       WorkOrders.description AS Description
 FROM WorkOrders 
 INNER JOIN Machines ON WorkOrders.machineId = Machines.machineId 
 INNER JOIN Locations ON WorkOrders.locationId = Locations.locationId;
@@ -110,16 +114,18 @@ SELECT locationId from Locations;
 -------------------------------------------------------
 
 -- display products associated with work order
-SELECT Products.productName, Products.productReference
+-- the current UI displays examples with workOrderId == 1, 2, 8
+SELECT Products.productName AS Product Name, Products.productReference AS Product Reference
     FROM WorkOrderProducts
     JOIN Products ON WorkOrderProducts.productId = Products.productId
-    WHERE WorkOrderProducts.workOrderId = workOrderId_from_input;
+    WHERE WorkOrderProducts.workOrderId = :workOrderId_from_input;
 
 -- display mechanics associated with work order
-SELECT Mechanics.firstName, Mechanics.lastName
+-- the current UI displays examples with workOrderId == 1, 2, 8
+SELECT Mechanics.firstName AS Mechanic First Name, Mechanics.lastName AS Mechanic Last Name
     FROM WorkOrderMechanics
     JOIN Mechanics ON WorkOrderMechanics.mechanicId = Mechanics.mechanicId
-    WHERE WorkOrderMechanics.workOrderId = workOrderId_from_input;
+    WHERE WorkOrderMechanics.workOrderId = :workOrderId_from_input;
 
 -- add product to work order (M-to-M relationship addition)
 INSERT INTO WorkOrderProducts (workOrderId, productId) VALUES (:workOrderId_from_input, :productId_from_input);
@@ -135,10 +141,49 @@ VALUES ((SELECT workOrderId FROM WorkOrders WHERE workOrderId = :workOrderId_fro
 -- delete mechanic from work order (M-to-M relationship deletion)
 DELETE FROM WorkOrderMechanics WHERE workOrderId = :workOrderId_from_the_update_form AND (SELECT mechanicId FROM Mechanics WHERE email=:emailInput);
 
+-- update WorkOrderProducts (M-to-M relationship update)
 UPDATE WorkOrderProducts SET productId = :productId_from_input WHERE workOrderId = :workOrderId_from_the_update_form AND productId = :productId_from_the_update_form;
 
--- populate productReference dropdown
+-- populate productReference dropdown with ids
 SELECT productId from Products;
 
--- populate mechanicEmail dropdown
+-- populate mechanicEmail dropdown with ids
 SELECT mechanicId from Mechanics;
+
+
+-------------------------------------------------------
+-- Work Order Products Detial Page
+-------------------------------------------------------
+
+-- display products associated with work order
+-- the current UI displays examples with workOrderId == 1, 2, 8
+SELECT Products.productName AS Product Name, Products.productReference AS Product Reference
+    FROM WorkOrderProducts
+    JOIN Products ON WorkOrderProducts.productId = Products.productId
+    WHERE WorkOrderProducts.workOrderId = :workOrderId_from_input;
+
+-- delete product from work order (M-to-M relationship deletion)
+DELETE FROM WorkOrderProducts WHERE workOrderId = :workOrderId_from_the_update_form AND (SELECT productId FROM Products WHERE reference = :referenceInput);
+
+-- add product to work order (M-to-M relationship addition)
+INSERT INTO WorkOrderProducts (workOrderId, productId) VALUES (:workOrderId_from_input, :productId_from_input);
+
+-- update WorkOrderProducts (M-to-M relationship update)
+UPDATE WorkOrderProducts SET productId = :productId_from_input WHERE workOrderId = :workOrderId_from_the_update_form AND productId = :productId_from_the_update_form;
+
+-------------------------------------------------------
+-- Work Order Mechanics Detial Page
+-------------------------------------------------------
+
+-- display mechanics associated with work order
+-- the current UI displays examples with mechanicId == 1, 2, 8
+SELECT Mechanics.firstName AS Mechanic First Name, Mechanics.lastName AS Mechanic Last Name
+    FROM WorkOrderMechanics
+    JOIN Mechanics ON WorkOrderMechanics.mechanicId = Mechanics.mechanicId
+    WHERE WorkOrderMechanics.workOrderId = :workOrderId_from_input;
+   
+-- delete mechanic from work order (M-to-M relationship deletion)
+DELETE FROM WorkOrderMechanics WHERE workOrderId = :workOrderId_from_the_update_form AND (SELECT mechanicId FROM Mechanics WHERE email=:emailInput);
+
+-- add mechanic to work order (M-to-M relationship addition)
+INSERT INTO WorkOrderMechanics (workOrderId, mechanicId) VALUES (:workOrderId_from_input, :mechanicId_from_input);
